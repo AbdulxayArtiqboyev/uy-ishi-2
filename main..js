dae377fa6ed692d6b24979b4d2a5d4c968cf6213
AbdulxayArtiqboyev <ggithub164@gmail.com>
2024-07-18 17:45:03 +0500

    uy ishi 2

---
+++ main..js
@@ -0,0 +1,96 @@
+// {
+//         // masala
+//         let  number = +prompt('pulingizni kiriting');
+//         if(number >= 9757983 ){
+//             console.log('oq yol Alisher');
+//         }
+//         else{
+//             console.log('Alisher ozgina sabr qilishi kerak bolar ekan');
+//         }
+//     }
+
+// {
+//     // 1 masala
+//     let number = +prompt('ixtiyoriy son kiriting');
+//     console.log(number + 1);
+// }
+
+// {
+//     // masala 2
+//     let number = +prompt('son kiriting');
+//     if(number > 0){
+//         console.log(`ushbu son musbat ${number}`);
+//     }
+//     else{
+//         console.log(`ushbu son manfiy ${number}`);
+//     }
+
+// }
+
+// {
+//     // masala 3
+//     let number = +prompt('son kiriting');
+//     if(number > 0 && number < 10){
+//         console.log(`1 xonali son ${number}`);
+//     }
+//     else if(number > 10 && number < 100){
+//         console.log(`ikki xonali son ${number}`);
+//     }
+//     else{
+//         console.log(`3 va undan kop son kiritdingiz ${number}`);
+//     }
+// }
+
+// {
+//     // masala 4
+//     let years = +prompt('yilingizni kiriting');
+//     let number = 2024;
+//     console.log(number - years);
+// }
+
+// {
+//     // masala 5
+//     let a = +prompt('a son kiriting');
+//     let b = +prompt('b son kiriting');
+//     if(a > b){
+//         console.log(a);
+//     }
+//     else{
+//         console.log(b);
+//     }
+// }
+
+// {
+//     // masala 6
+//     let a = +prompt('a son kiriting');
+//     let b = +prompt('b son kiriting');
+//     let c = +prompt('c son kiriting');
+//     if(a > b && a > c){
+//         console.log(`eng katta son ${a}`);
+//     }
+//     else if(b > a && b > c){
+//         console.log(`eng katta son ${b}`);
+//     }
+//     else{
+//         console.log(`eng katta son ${c}`);
+//     }
+// }
+
+// {
+//     // masala 7
+//     let a = +prompt('a son kiriting');
+//     let b = +prompt('b son kiriting');
+//     let c = +prompt('c son kiriting');
+//     if(a < b && a < c){
+//         console.log(`eng kichik son ${a}`);
+//     }
+//     else if(b < a && b < c){
+//         console.log(`eng kichik son ${b}`);
+//     }
+//     else{
+//         console.log(`eng kichik son ${c}`);
+//     }
+
+// }
+
+// 
